c375ab959744fc0112ac4a77ab1c1f92896dd07d
Patrick Rice <patrickrice20@gmail.com>
2020-05-04 21:46:56 -0700

    Add clone if repo is not cloned

--- install.sh
+++ install.sh
@@ -8,9 +8,21 @@ if ! [ -x "$(command -v curl)" ]; then
   sudo apt install curl -y
 fi
 
-if ! [ -x "$(command -v curl)" ]; then
+if ! [ -x "$(command -v ansible)" ]; then
   sudo apt install ansible -y
 fi
+
+if ! [ -x "$(command -v git)" ]; then
+  sudo apt install git -y
+fi
+
+INSTALL_FOLDER=~/setup
+
+if [ ! -d "${INSTALL_FOLDER}" ] ; then
+    git clone "git@github.com:pattrickrice/desktop_setup.git" "$INSTALL_FOLDER"
+fi
+
+cd ${INSTALL_FOLDER}
 
 sudo chmod 777 /etc/ansible/hosts
 echo "[localhost]
